2a743a9f941f087a7870ac302c959a54c1065393
Igor <Aleshichev.git@email.ua>
2022-06-29 11:43:58 +0300

    forms commit

--- src/cities/models.py
+++ src/cities/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
 
 class City(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Город')
@@ -12,3 +14,6 @@ class City(models.Model):
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
         ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse('cities:detail', kwargs={'pk': self.pk})
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 
-# Create your views here.
+from cities.forms import HtmlForm, CityForm
+from cities.models import City
+
+__all__=(
+    'home', 'CityDetailView', 'CityCreateView',
+)
+def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+
+    # if pk:
+    #     # city = City.objects.filter(id=pk).first()
+    #     # city = City.objects.get(id=pk)
+    #     city = get_object_or_404(City, id=pk)
+    #
+    #     context = {'object': city}
+    #     return render(request, 'cities/detail.html', context)
+    form = CityForm()
+    qs = City.objects.all()
+    context = {'objects_list': qs, 'form': form}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
